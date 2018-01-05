7d18148402a72e2497ffd7e441a1dfab6e578bf1
stonelqf <876096545@qq.com>
2018-01-05 10:33:17 +0800

    完善中间件代理池的设置

--- douban/mymiddlewares.py
+++ douban/mymiddlewares.py
@@ -1,9 +1,24 @@
 import random
+# 导入配置文件
 from scrapy.conf import settings
+import base64
 
 
 class MyUserAgent(object):
-   def process_request(self,request,spider):
-      useragent=random.choice(settings["USER_AGENTS"])
-      # print(useragent)
-      request.headers.setdefault("User-Agent",useragent)
+    def process_request(self, request, spider):
+        useragent = random.choice(settings["USER_AGENTS"])
+        # print(useragent)
+        request.headers.setdefault("User-Agent", useragent)
+
+
+class MyProxyAgent(object):
+    def process_request(self, request, spider):
+        proxy = random.choice(settings["PROXYS"])
+        # print(proxy)
+        # Set the loca、tion of the proxy
+        request.meta['proxy'] = proxy['ip']
+        if len(proxy['passwd']) != 0:
+            # 很多网上的答案使用base64.encodestring来编码proxy_user_pass，有一种情况，当username太长的时候，会出现错误，所以推荐使用b64encode编码方式
+            encodedPasswd = base64.b64encode(proxy['passwd'])
+            # 有些代理是有账户和密码的，这时候就需要验证
+            request.headers['Proxy_Authorization'] = 'Basic ' + encodedPasswd
--- douban/settings.py
+++ douban/settings.py
@@ -14,29 +14,28 @@ BOT_NAME = 'douban'
 SPIDER_MODULES = ['douban.spiders']
 NEWSPIDER_MODULE = 'douban.spiders'
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'douban (+http://www.yourdomain.com)'
+# USER_AGENT = 'douban (+http://www.yourdomain.com)'
 USER_AGENT = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;"
 # Obey robots.txt rules
 # ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
 # DEFAULT_REQUEST_HEADERS = {
@@ -45,59 +44,64 @@ DOWNLOAD_DELAY = 3
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'douban.middlewares.DoubanSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
-   'douban.mymiddlewares.MyUserAgent': 543,
+    'douban.mymiddlewares.MyUserAgent': 100,
+    'douban.mymiddlewares.MyProxyAgent': 200,
 }
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'douban.pipelines.DoubanPipeline': 300,
+    'douban.pipelines.DoubanPipeline': 300,
 }
 # MongoDB ip
-MONGODB_HOST="127.0.0.1"
+MONGODB_HOST = "127.0.0.1"
 # MongoDB 端口
-MONGODB_PORT=27017
+MONGODB_PORT = 27017
 # 数据库名
-MONGODB_NAME="douban"
+MONGODB_NAME = "douban"
 # 表名称
-MONGODB_TABLE_NAME="doubanmovie"
+MONGODB_TABLE_NAME = "doubanmovie"
 
-USER_AGENTS=[
-   "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;",
-   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
-   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
-   " Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)",
+USER_AGENTS = [
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+    " Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)",
+]
+PROXYS = [
+    {"ip": "http://1118.193.107.115:80", "passwd": ""},
+    {"ip": "http://111.231.25.167:8888", "passwd": ""},
 ]
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
